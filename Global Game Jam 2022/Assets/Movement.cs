e34fe5f0bcf60d6e6dae90854cc852857dbb49d4
Kye <50266459+albedoDeveloper@users.noreply.github.com>
2022-01-29 12:03:47 +0800

    Fixed player

--- Global Game Jam 2022/Assets/Movement.cs
+++ Global Game Jam 2022/Assets/Movement.cs
@@ -10,14 +10,14 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Camera.main.GetComponent<CameraStart>().SetChild(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-            gameObject.transform.Translate(new Vector3(0.1f, 0, 0));
+        if (Input.GetKey(KeyCode.A))
+            gameObject.transform.Translate(new Vector3(0.01f, 0, 0));
        /* if(Input.GetKeyDown(KeyCode.B))
         {
             ColourChange();
