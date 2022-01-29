d2e9da6b9fbd456c409b6de562f7ffe406b67a11
Robert <38125987+robertvalentic@users.noreply.github.com>
2022-01-29 22:19:42 +0800

    changes

--- Global Game Jam 2022/Assets/Scripts/BuildController.cs
+++ Global Game Jam 2022/Assets/Scripts/BuildController.cs
@@ -2,20 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LampController : MonoBehaviour
+public class BuildController : MonoBehaviour
 {
-    public CircleCollider2D _circleCollider;
-    Vector3[] _positions = new Vector3[2];
+    public GameObject _lightPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+
+        }
     }
 }
